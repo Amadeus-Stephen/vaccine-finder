e6cae1f5721887ebed8e5f850c3d5c50086ae8b1
Robel Schwarz <itsrobel.schwarz@gmail.com>
2021-03-08 22:21:49 -0500

    added search and reverse geocoding

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -1,12 +1,13 @@
 <template>
 	<div ref="map-root" style="width:100%; height:100%">
 	</div>
+	
 </template>
 <script>
 import 'ol/ol.css';
-import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import Geocoder from "ol-geocoder";
 import {OSM, Vector as VectorSource} from 'ol/source';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {fromLonLat} from 'ol/proj';
@@ -15,35 +16,6 @@ import {fromLonLat} from 'ol/proj';
 
 import "ol/ol.css"
 let source = new VectorSource();
-fetch('data/geojson/roads-seoul.geojson')
-  .then(function (response) {
-	return response.json();
-  })
-  .then(function (json) {
-	var format = new GeoJSON();
-	var features = format.readFeatures(json);
-	var street = features[0];
-
-	// convert to a turf.js feature
-	var turfLine = format.writeFeatureObject(street);
-
-	// show a marker every 200 meters
-	var distance = 0.2;
-
-	// get the line length in kilometers
-	var length = turf.lineDistance(turfLine, 'kilometers');
-	for (var i = 1; i <= length / distance; i++) {
-	  var turfPoint = turf.along(turfLine, i * distance, 'kilometers');
-
-	  // convert the generated point to a OpenLayers feature
-	  var marker = format.readFeature(turfPoint);
-	  marker.getGeometry().transform('EPSG:4326', 'EPSG:3857');
-	  source.addFeature(marker);
-	}
-
-	street.getGeometry().transform('EPSG:4326', 'EPSG:3857');
-	source.addFeature(street);
-})
 let vectorLayer = new VectorLayer({
   source: source,
 });
@@ -51,67 +23,62 @@ let vectorLayer = new VectorLayer({
 let rasterLayer = new TileLayer({
   source: new OSM(),
 });
-let  coords = [0 , 0];
+
 export default {
 	name:"MapComtainer", 
 	components: {},
-   	beforeMount() {
- 	  	let coords = [0 , 0];
-  		// function success(position) {
-		// 	const latitude  = position.coords.latitude;
-		// 	const longitude = position.coords.longitude;
-		// 	return [longitude, latitude]
- 	 	// }
- 	 	// function error() {
-		// 	status.textContent = 'Unable to retrieve your location';
- 		// }
-
- 	 	// if(!navigator.geolocation) 
-		// {
-		//  	alert("Geoloaction is not supported in your browser")
- 	 	// }
-		// else
-		// {
-		// 	navigator.geolocation.getCurrentPosition((position) => 
-		// 	{
-		// 		coords = success(position)
-		// 		console.log(coords)
-		// 	}, error);
- 		// }
-		
-		// function getPosition() {
-    	// 	return new Promise((res, rej) => {
-     	//    		navigator.geolocation.getCurrentPosition(res, rej);	
-
-
-    	// 	}).then((data) => {
-		// 		return [long , lat]
-		// 	})
-		// }
-		let coordPromise = {then: function(resolve) {
-			new Promise((res , rej) => {
-				navigator.geolocation.getCurrentPosition(res, rej)
-			}).then(data => {
-            	let lat = data.coords.latitude
-            	let long = data.coords.longitude
-				resolve([long, lat])
-			})	
-		}}
-		coords = Promise.resolve(coordPromise)
-		console.log(Promise.resolve(coords))
-	},
    	mounted() {
-		new Map({
-		   target: this.$refs["map-root"],
-		   layers: [
-			   rasterLayer , vectorLayer
-		   ],
-		   view: new View({
-			   zoom:15,
-			   center: fromLonLat(coords),
-			   constrainResolution:true
-		   	})
-	   	})
-   	}
+		
+			 navigator.geolocation.getCurrentPosition((data)=>{
+				 console.log("return")
+				 console.log(data)
+				 console.log(data.coords.longitude , data.coords.latitude)
+				fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client\?latitude\=${data.coords.latitude}\&longitude\=${data.coords.longitude}\&localityLanguage\=en`)
+				.then(response => response.json())
+				.then(geodata => console.log(geodata))
+				let map = new Map({
+		   			target: this.$refs["map-root"],
+		   			layers: [
+			   			rasterLayer , vectorLayer
+		   			],
+		   			view: new View({
+			   			zoom:15,
+			   			center: fromLonLat([data.coords.longitude, data.coords.latitude]),
+			  			constrainResolution:true
+		   			}),
+				
+	   			})
+				//Instantiate with some options and add the Control
+				let geocoder = new Geocoder('nominatim', {
+				  provider: 'osm',
+				  lang: 'en',
+				  placeholder: 'Search for ...',
+				  targetType: 'text-input',
+				  limit: 5,
+				  debug: false,
+				  autoComplete: true,
+				  keepOpen: true
+				});
+				map.addControl(geocoder);
+				
+				//Listen when an address is chosen
+				geocoder.on('addresschosen', function (evt) {
+					console.info(evt);
+				});
+			 }, 
+			 () => {
+				new Map({
+		   			target: this.$refs["map-root"],
+		   			layers: [
+			   			rasterLayer , vectorLayer
+		   			],
+		   			view: new View({
+			   			zoom:0,
+			   			center: fromLonLat([0,0]),
+			  			constrainResolution:true
+		   			})
+	   			})
+			 })
+		 }
 }
 </script>
