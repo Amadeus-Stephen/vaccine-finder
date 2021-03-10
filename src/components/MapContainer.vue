cedc31a54e5e2b8975212454a803c1b00a50475c
Robel Schwarz <itsrobel.schwarz@gmail.com>
2021-03-09 21:12:09 -0500

    commit

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -7,10 +7,12 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import Feature from 'ol/Feature'
+import Polygon from "ol/geom/Polygon"
 import Geocoder from "ol-geocoder";
 import {OSM, Vector as VectorSource} from 'ol/source';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
-import {fromLonLat} from 'ol/proj';
+import {fromLonLat, transform} from 'ol/proj';
 
 
 
@@ -48,6 +50,16 @@ export default {
 		   			}),
 				
 	   			})
+				var thing = new Polygon( [[
+   					transform([-16,-22], 'EPSG:4326', 'EPSG:3857'),
+    				transform([-44,-55], 'EPSG:4326', 'EPSG:3857'),
+    				transform([-88,75], 'EPSG:4326', 'EPSG:3857')
+				]]);
+				var featurething = new Feature({
+    				name: "Thing",
+    				geometry: thing
+				});
+				source.addFeature( featurething );
 				//Instantiate with some options and add the Control
 				let geocoder = new Geocoder('nominatim', {
 				  provider: 'osm',
